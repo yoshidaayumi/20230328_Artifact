c5843d6dfac727df13d1b478546bf31520cc1ac1
yoshidaayumi <201184k@tokyo.fuji.ac.jp>
2023-04-03 22:47:17 +0900

    postcontrollerの作成

---
+++ database/migrations/2023_04_03_130858_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Post;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+    
+    public function index(Post $post)
+    {
+        return $post->get();
+    }
+};
